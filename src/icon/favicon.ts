48577d00b53649081d27a7c7cbba122d12e9e6b2
Thomas Paul Mann <hello@thomaspaulmann.com>
2022-08-02 22:09:24 +0100

    Favicons: Add docs and options

--- src/icon/favicon.ts
+++ src/icon/favicon.ts
@@ -1,13 +1,39 @@
 import { Icon, Image } from "@raycast/api";
 import { URL } from "url";
 
-export function getFavicon(url: string | URL, size = 64): Image.ImageLike {
+/**
+ * Icon to represent the progress of _something_.
+ *
+ * @param progress The URL of the website to represent.
+ *
+ * @returns an Image that can be used where Raycast expects them.
+ *
+ * @example
+ * ```
+ * <List.Item icon={getFavicon("https://raycast.com")} title="Raycast Website" />
+ * ```
+ */
+export function getFavicon(
+  url: string | URL,
+  options?: {
+    /**
+     * Size of the Favicon
+     * @default 64
+     */
+    size?: number;
+    /**
+     * Fallback icon in case the Favicon is not found.
+     * @default Icon.Link
+     */
+    fallback?: Image.Fallback;
+  }
+): Image.ImageLike {
   try {
     const urlObj = typeof url === "string" ? new URL(url) : url;
     const hostname = urlObj.hostname;
     return {
-      source: `https://www.google.com/s2/favicons?sz=${size}&domain=${hostname}`,
-      fallback: Icon.Link,
+      source: `https://www.google.com/s2/favicons?sz=${options?.size ?? 64}&domain=${hostname}`,
+      fallback: options?.fallback ?? Icon.Link,
     };
   } catch (e) {
     console.error(e);
