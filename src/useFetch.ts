b96af8aa2bffc2023ddb27c6d08299dccf07804c
Sorin Muntean <me@sorinmuntean.ro>
2024-03-06 15:55:24 +0100

    add docs for `useFetch`

--- src/useFetch.ts
+++ src/useFetch.ts
@@ -50,6 +50,52 @@ function defaultMapping<V, T extends unknown[]>(result: V): { data: T; hasMore?:
 
 type PaginatedRequestInfo = (pagination: { page: number; lastItem?: any }) => RequestInfo;
 
+/**
+ * Fetches the paginatedURL and returns the {@link AsyncState} corresponding to the execution of the fetch. The last value will be kept between command runs.
+ *
+ * @remark This overload should be used when working with paginated data sources.
+ * @remark When paginating, only the first page will be cached.
+ *
+ * @example
+ * ```
+ * import { Icon, Image, List } from "@raycast/api";
+ * import { useFetch } from "@raycast/utils";
+ * import { useState } from "react";
+ *
+ * type SearchResult = { companies: Company[]; page: number; totalPages: number };
+ * type Company = { id: number; name: string; smallLogoUrl?: string };
+ * export default function Command() {
+ *   const [searchText, setSearchText] = useState("");
+ *   const { isLoading, data, pagination } = useFetch(
+ *     (options) =>
+ *       "https://api.ycombinator.com/v0.1/companies?" +
+ *       new URLSearchParams({ page: String(options.page + 1), q: searchText }).toString(),
+ *     {
+ *       mapResult(result: SearchResult) {
+ *         return {
+ *           data: result.companies,
+ *           hasMore: result.page < result.totalPages,
+ *         };
+ *       },
+ *       keepPreviousData: true,
+ *       initialData: [],
+ *     },
+ *   );
+ *
+ *   return (
+ *     <List isLoading={isLoading} pagination={pagination} onSearchTextChange={setSearchText}>
+ *       {data.map((company) => (
+ *         <List.Item
+ *           key={company.id}
+ *           icon={{ source: company.smallLogoUrl ?? Icon.MinusCircle, mask: Image.Mask.RoundedRectangle }}
+ *           title={company.name}
+ *         />
+ *       ))}
+ *     </List>
+ *   );
+ * }
+ * ```
+ */
 export function useFetch<V = unknown, U = undefined, T extends unknown[] = unknown[]>(
   url: PaginatedRequestInfo,
   options: RequestInit & {
