de089af0700bb7ef63173424cbcd6d30dedf6320
Sorin Muntean <me@sorinmuntean.ro>
2024-03-13 11:27:45 +0100

    Add `useJSON` hook, to allow streaming through JSON files.

--- src/useFetch.ts
+++ src/useFetch.ts
@@ -1,36 +1,10 @@
 import { useCallback, useMemo, useRef } from "react";
 import hash from "object-hash";
-import mediaTyper from "media-typer";
-import contentType from "content-type";
 import { useCachedPromise, CachedPromiseOptions } from "./useCachedPromise";
 import { useLatest } from "./useLatest";
 import { FunctionReturningPaginatedPromise, FunctionReturningPromise, UseCachedPromiseReturnType } from "./types";
 import { fetch } from "cross-fetch";
-
-function isJSON(contentTypeHeader: string | null | undefined): boolean {
-  if (contentTypeHeader) {
-    const ct = contentType.parse(contentTypeHeader);
-
-    const mediaType = mediaTyper.parse(ct.type);
-
-    if (mediaType.subtype === "json") {
-      return true;
-    }
-
-    if (mediaType.suffix === "json") {
-      return true;
-    }
-
-    if (mediaType.suffix && /\bjson\b/i.test(mediaType.suffix)) {
-      return true;
-    }
-
-    if (mediaType.subtype && /\bjson\b/i.test(mediaType.subtype)) {
-      return true;
-    }
-  }
-  return false;
-}
+import { isJSON } from "./fetch-utils";
 
 async function defaultParsing(response: Response) {
   if (!response.ok) {
