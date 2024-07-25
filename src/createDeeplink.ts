85075d43f04b7c8ea06ae98d164be01748cd8ef8
erics118 <52634785+erics118@users.noreply.github.com>
2024-07-25 10:16:06 -0400

    feat: support extension query params

--- src/createDeeplink.ts
+++ src/createDeeplink.ts
@@ -3,16 +3,66 @@ import { environment } from "@raycast/api";
 export type DeeplinkType = "extension" | "script-command"
 
 export type CreateScriptCommandDeeplinkOptions = {
+  /**
+   * The type of deeplink, which should be "script-command".
+   */
   type: "script-command",
+  /**
+   * The name of the command.
+   */
   command: string,
-  arguments: string[],
+  /**
+   * If the command accepts arguments, they can be passed using this query parameter.
+   */
+  arguments?: string[],
 };
 
+/**
+ * Options for creating a deeplink to an extension.
+ */
 export type CreateExtensionDeeplinkOptions = {
+  /**
+   * The type of deeplink, which should be "extension".
+   */
   type: "extension",
+  /**
+   * The name of the owner or author of the extension.
+   */
   ownerOrAuthorName: string,
+  /**
+   * The name of the extension.
+   */
   extensionName: string,
+  /**
+   * The command associated with the extension.
+   */
   command: string,
+  /**
+   * Either "userInitiated", which runs the command in the foreground, or "background", which skips bringing Raycast to the front.
+   */
+  launchType?: "userInitiated" | "background",
+  /**
+   * If the command accepts arguments, they can be passed using this query parameter.
+   */
+  arguments?: {
+    /**
+     * The representation of arguments given that key here is the `name` defined in manifest file and value is the user's input
+     */
+    [item: string]: any;
+  },
+  /**
+   * If the command make use of LaunchContext, it can be passed using this query parameter.
+   */
+  context?: {
+    /**
+     * The context values for a command launch.
+     */
+    [item: string]: any;
+  },
+  /**
+   * Some text to prefill the search bar or first text input of the command
+   */
+  fallbackText?: string,
 };
 
 export type CreateDeeplinkOptions = CreateScriptCommandDeeplinkOptions | CreateExtensionDeeplinkOptions;
@@ -37,6 +87,24 @@ export function createScriptCommandDeeplink(options: CreateScriptCommandDeeplink
 
 export function createExtensionDeeplink(options: CreateExtensionDeeplinkOptions): string {
   const protocol = getProtocol();
+
+  const params = new URLSearchParams();
+
+  if (options.launchType) {
+    params.append("launchType", options.launchType);
+  }
+
+  if (options.arguments) {
+    params.append("arguments", JSON.stringify(options.arguments));
+  }
+
+  if (options.context) {
+    params.append("context", JSON.stringify(options.context));
+  }
+
+  if (options.fallbackText) {
+    params.append("fallbackText", options.fallbackText);
+  }
 
   return `${protocol}extensions/${options.ownerOrAuthorName}/${options.extensionName}/${options.command}`;
 }
