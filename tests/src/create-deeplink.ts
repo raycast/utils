96981fc98d9905493bce6d2cc2e8abf70ce086e7
erics118 <52634785+erics118@users.noreply.github.com>
2024-08-14 12:22:04 -0400

    chore: lint, don't open deeplink

--- tests/src/create-deeplink.ts
+++ tests/src/create-deeplink.ts
@@ -1,38 +1,43 @@
-import { createDeeplink, DeeplinkType } from "../../src/index";
+import { createDeeplink, DeeplinkType } from "@raycast/utils";
 import { exec } from "node:child_process";
 
-function testDeeplink(deeplink: string) {
+function testDeeplink(deeplink: string, run = false) {
   console.log(deeplink);
-  exec(`open ${deeplink}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+  if (run) {
+    exec(`open ${deeplink}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  }
 }
 
 export default async function () {
   // raycast://script-commands/count-chars?arguments=a%20b%2Bc%2520d
-  testDeeplink(createDeeplink({
-    type: DeeplinkType.ScriptCommand,
-    command: "count-chars",
-    arguments: ["a b+c%20d"]
-  }))
+  testDeeplink(
+    createDeeplink({
+      type: DeeplinkType.ScriptCommand,
+      command: "count-chars",
+      arguments: ["a b+c%20d"],
+    }),
+  );
 
   // raycast://extensions/Aayush9029/cleanshotx/capture-previous-area?arguments=%7B%22action%22%3A%22copy%22%7D
-  // testDeeplink(createDeeplink({
-  //   type: DeeplinkType.Extension,
-  //   ownerOrAuthorName: "Aayush9029",
-  //   extensionName: "cleanshotx",
-  //   command: "capture-previous-area",
-  //   arguments: {
-  //     action: "copy"
-  //   }
-  // }))
-
+  testDeeplink(
+    createDeeplink({
+      type: DeeplinkType.Extension,
+      ownerOrAuthorName: "Aayush9029",
+      extensionName: "cleanshotx",
+      command: "capture-previous-area",
+      arguments: {
+        action: "copy",
+      },
+    }),
+  );
 }
